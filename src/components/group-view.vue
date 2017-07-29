17a76e2c9ff416fa3657232736ff56a4ba7a6978
Jono Mingard <reason.koan@gmail.com>
2017-07-29 16:55:56 +1200

    started styling groups and stats

--- src/components/group-view.vue
+++ src/components/group-view.vue
@@ -1,36 +1,45 @@
 <template>
-<div class="hello">
-  <div v-for='group in groups'><h2>{{group.title}}</h2>
-  <div v-for='item in group.items'> {{item.title}}
-  <button
-    v-for='option in item.options'
-    :class="{ active: option.value === item.current }"
-    @click='setCurrentOption(option, item, group) '>
-  {{option.title}}
-  </button>
+  <div class="controls">
+    <div v-for='group in groups' :key="group.title" class="group">
+      <h2 class="group-title">{{group.title}}</h2>
+      <div class="item" v-for='item in group.items' :key="item.title">
+        <h3 class="item-title">{{item.title}}</h3>
+        <button
+            v-for='option in item.options'
+            :key="option.title"
+            class="option"
+            :class="{ active: option.value === item.current }"
+            @click='setCurrentOption(option, item, group) '>
+          {{option.title}}
+        </button>
+      </div>
+    </div>
   </div>
-  </div>
-
-</div>
 </template>
 
 <script>
 export default {
-  name: 'hello',
+  name: 'group-view',
   computed: {
-  groups () {
-  return this.$store.state.groups
-  }
+    groups() {
+      return this.$store.state.groups
+    }
   },
   methods: {
-  setCurrentOption(option, item, group) {
-  this.$store.commit('setCurrentOption', {option , item, group }) 
-  }
+    setCurrentOption(option, item, group) {
+      this.$store.commit('setCurrentOption', { option, item, group })
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.controls {
+  background-color: white;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.25);
+  padding: 10px;
+  width: 25%;
+  max-width: 400px;
+}
 </style>
--- src/components/stats.vue
+++ src/components/stats.vue
@@ -1,11 +1,25 @@
 <template>
-<div>
-  <div>{{ money }}</div>
-  <div>{{ time }}</div>
-  <div>{{ enviroImpact }}</div>
+<div class="stats-wrapper">
+  <div class="stats">
+    <div class="stat">
+      <img src="../assets/coins.png" />
+      <span :class="money > 0 ? 'positive' : 'negative'">{{ money }}</span>
+    </div>
+    <div class="stat">
+      <img src="../assets/timer.png" />
+      <span :class="time > 0 ? 'positive' : 'negative'">{{ time }}</span>
+    </div>
+    <div class="stat">
+      <img src="../assets/grass.png" />
+      <span :class="enviroImpact > 0 ? 'positive' : 'negative'">{{ enviroImpact }}</span>
+    </div>
+  </div>
 
+  <div class="actions">
+    <button class="action">What does this mean?</button>
+    <button class="action">Where are the stats from?</button>
+  </div>
 </div>
-
 </template>
 
 <script>
@@ -41,5 +55,71 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.stats-wrapper {
+  align-self: center;
+  margin: 0 auto;
+  background-color: #444;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+}
 
+.stats {
+  display: flex;
+  justify-content: center;
+  padding: 10px 20px;
+  background-color: #333;
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+}
+
+.stat {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: #ddd;
+}
+
+.stat.positive {
+  color: #8BC34A;
+}
+
+.stat.negative {
+  color: #DD2C00;
+}
+
+.stat > img {
+  width: 72px;
+  height: 72px;
+}
+
+.stat > span {
+  font-size: 3rem;
+}
+
+.actions {
+  display: flex;
+}
+
+.action {
+  margin: 10px;
+  padding: 5px 15px;
+  background: transparent;
+  border: none;
+  border-radius: 5px;
+  color: #B3E5FC;
+  transition: all 0.2s;
+}
+
+.action:not(:last-child) {
+  margin-right: 0;
+}
+
+.action:hover {
+  background-color: rgba(179, 229, 252, 0.2);
+}
+
+.action:active {
+  background-color: rgba(179, 229, 252, 0.4);
+}
 </style>
