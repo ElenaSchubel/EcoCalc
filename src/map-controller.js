c21ebf911fbf49cd5ca1f010b447c756349f8f3e
ElenaSchubel <ebassist@live.com>
2017-07-30 12:57:34 +1200

    added some icons

--- src/map-controller.js
+++ src/map-controller.js
@@ -72,11 +72,22 @@ export default class MapController {
   }
 
   addMarkersToMap() {
+    var riverIcon = new H.map.Icon('static/river.png')
+    var riverMarker = new H.map.Marker({lng:174.7442 	, lat:-41.299 }, {icon: riverIcon})
+    var ticketIcon = new H.map.Icon('static/ticket1.png')
+    var ticketMarker =  new H.map.Marker({lng:174.775384, lat:-41.293715}, {icon: ticketIcon})
     var landfillIcon = new H.map.Icon('static/landfill.png');
     var landfillMarker = new H.map.Marker({lng: 174.745253, lat: -41.325648}, { icon: landfillIcon });
-    this.map.addObject(landfillMarker);
+    var marineIcon = new H.map.Icon('static/dolphin.png');
+    var marineMarker = new H.map.Marker({lng:  174.774226, lat:-41.345420}, { icon: marineIcon });
+    var boatIcon = new H.map.Icon('static/boat.png');
+    var boatMarker = new H.map.Marker({lng:174.782003 ,lat:-41.279916}, { icon: boatIcon });
+    this.map.addObject(boatMarker)
+    this.map.addObject(marineMarker)
+    this.map.addObject(ticketMarker)
+    this.map.addObject(riverMarker)
+    this.map.addObject(landfillMarker)
   }
-
   restrictMap() {
     var bounds = new H.geo.Rect(-41.200035, 174.586065, -41.359314, 174.889477 );
 
